b5619a3256e8820989b7c56eb5d59f023e1c27cd
handgun50 <gm_cr1m3@yahoo.com>
2016-01-03 21:39:31 +0700

    insta 7.13.1

--- app/src/main/java/org/ollide/xposed/instagram/ClassNames.java
+++ app/src/main/java/org/ollide/xposed/instagram/ClassNames.java
@@ -40,9 +40,11 @@ public final class ClassNames {
     private static final int V14_7_5_2 = 14547085; // Sep 04 '15
 
     private static final int V14_7_6_0 = 14592506; // Sep 04 '15
+    
+    private static final int V14_7_13_1 = 19276016; // Dec 21 '15
 
     private static final Integer[] V14_CODES = {V14_6_20_0, V14_6_20_1, V14_6_20_1_2, V14_6_20_2,
-            V14_7_3_0, V14_7_4_0, V14_7_5_0, V14_7_5_1, V14_7_5_2, V14_7_6_0};
+            V14_7_3_0, V14_7_4_0, V14_7_5_0, V14_7_5_1, V14_7_5_2, V14_7_6_0, V14_7_13_1};
 
     /**
      * Last changed with version 6.19.0 (8582325) last checked with 9350372
@@ -82,7 +84,10 @@ public final class ClassNames {
         }
 
         // minSdk14 class names
-        if (versionCode >= V14_7_6_0) {
+        if (versionCode >= V14_7_13_1) {
+            v14DoubleTabListenerName = "com.instagram.android.feed.a.b.g";
+            v14HeartIconTapListenerName = "com.instagram.android.feed.a.b.af";
+        } else if (versionCode >= V14_7_6_0) {
             v14DoubleTabListenerName = "com.instagram.android.feed.a.b.ax";
             v14HeartIconTapListenerName = "com.instagram.android.feed.a.b.z";
         } else if (versionCode >= V14_7_4_0) {
